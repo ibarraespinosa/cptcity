3aef7d266148cc31f57b067c3d1f763567815af1
ibarraespinosa <zergioibarra@gmail.com>
2018-02-27 10:01:33 -0300

    pre CRAN

--- R/cpt.R
+++ R/cpt.R
@@ -1,36 +1,25 @@
-#' Function to return colour palletes functions from cpt-city
+#' Function to return colour palletes functions from 'cpt-city'
 #'
 #' This function return a collor pallete based on the name or position of the
-#' pallete. Currently there are three available palletes:
+#' pallete.
 #'
-#' \enumerate{
-#'   \item "inferno"
-#'   \item "temperature"
-#'   \item "sst"
-#'   \item "temp"
-#'   \item "arendarl_arctic"
-#'   \item "arendarl_temperature"
-#'   \item "arendarl_zambezi_focus"
-#'   \item "arendarl_zambezi_proximity"
-#' }
-#'
-#' The palletes are ones of the most popular.
-#'
-#' The palletes are available here:
+#' The cpt-city web archive comes from:
 #' http://soliton.vm.bytemark.co.uk/pub/cpt-city/index.html
 #'
 #' @param pal Pallete of colors available or the number of the position
 #' @param n integer; number of colors
 #' of the same
-#' @param ... ignored
 #'
 #' @return A colour pallete function.
 #' @importFrom grDevices rgb colorRampPalette
 #' @export
-#' @examples \dontrun{
-#' # Do not run
+#' @examples {
 #' library(cptcity)
-#' image(matrix(1:100), col = cpt(pal = "inferno"))
+#' image(matrix(1:100), col = cpt(pal = "mpl_inferno"))
+#' find_cpt("temperature")
+#' image(matrix(1:100), col = cpt("idv_temperature"))
+#' \dontrun{
+#' # Do not run
 #' library(ggplot2)
 #' ggplot(faithfuld, aes(waiting, eruptions)) +
 #' geom_raster(aes(fill = density))
@@ -39,7 +28,8 @@
 #'  geom_raster(aes(fill = density)) +
 #'  scale_fill_gradientn(colours = cpt(n = 100))
 #' }
-cpt <- function(pal = "mpl_inferno", n = 100,  ...){
+#' }
+cpt <- function(pal = "mpl_inferno", n = 100){
   m <- sysdata[[pal]]
   col <-  rgb(red = m$r, green = m$g, blue = m$b,
               maxColorValue = max(c(max(m$r, na.rm = T),
--- R/find_cpt.R
+++ R/find_cpt.R
@@ -4,14 +4,16 @@
 #' the search. It is a searcher. It is a mini mini mini google.
 #'
 #' @param name character; Word to be searched among the names of the cpt gradients.
-#' @param ... ignored
-#'
 #' @return names that satisfy the search.
 #' @note This functions runs grep.
 #' @export
-#' @examples \dontrun{
-#' # Do not run
+#' @examples {
 #' library(cptcity)
+#' find_cpt("temperature")
+#' image(matrix(1:100), col = cpt("idv_temperature"))
+#' \dontrun{
+#' library(cptcity)
+#' # Do not run
 #' # data names_cpt lazy loaded, already in environment
 #' library(ggplot2)
 #' ggplot(faithfuld, aes(waiting, eruptions)) +
@@ -27,8 +29,9 @@
 #' geom_raster(aes(fill = density)) +
 #' scale_fill_gradientn(colours = cpt(pal = "pj_1_a_rainbow"))
 #' }
-find_cpt <- function(name, ...){
+#' }
+find_cpt <- function(name){
   cpt_names <- sysdata[["cpt_names"]]
-  y <- cpt_names[grep(pattern = name, x = cpt_names, ...)]
+  y <- cpt_names[grep(pattern = name, x = cpt_names)]
 return(y)
 }
